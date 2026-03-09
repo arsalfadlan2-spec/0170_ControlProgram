1ad3c620833b85d5695ee5f3684cc8ccd61d5f9b
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-03-09 17:37:30 +0700

    menambahkan kategori nilai

--- file2.cpp
+++ file2.cpp
@@ -11,6 +11,16 @@ string Status_lulus(float r){
     else
         return "gagal";
 }
+string kategori_nilai(float r){
+        if (r >= 90)
+             return "A (Sangat Baik)";
+        else if (r >= 80)
+            return "B (Baik)";
+        else if (r >= 70)
+            return "C (Cukup)";
+        else 
+            return "D (Kurang)";
+}
     
 int main(){
     float nilBI,nilMT,rerata;
@@ -21,14 +31,24 @@ int main(){
     cout <<"masukkan nilai Matematika = ";
     cin >> nilMT;
 
+    string kategori;
+
+    cout << "Rata-rata nilai = " << rerata << endl;
+    cout << "Status = " << status << endl;
+    cout << "Kategori Nilai = " << kategori << endl;
+
+    
     //rerata =(nilbi+nilMT)/2;
     //if (rerata >= 60)
         //  Status = "lolos";
     //else
         // status = "gagal";
     rerata = Rata_rata(nilBI,nilMT);
+    status = Status_lulus(rerata);
+    kategori = kategori_nilai(rerata);
     // Status = Status_lulus(rerata)
 
     cout << "status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+    cout << "Perlu belajar lebih giat!" << endl;
 
 }
