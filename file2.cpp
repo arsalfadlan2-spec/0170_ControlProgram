e0a420197604fb6cdbac4fda0bf96db588b1e581
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-03-09 17:22:45 +0700

    Membuat Library file 2

---
+++ file2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a,float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "lolos";
+    else
+        return "gagal";
+}
+    
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout <<"masukkan Nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata =(nilbi+nilMT)/2;
+    //if (rerata >= 60)
+        //  Status = "lolos";
+    //else
+        // status = "gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    // Status = Status_lulus(rerata)
+
+    cout << "status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+
+}
